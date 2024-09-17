7fedfad7a89772ab2db763f392ac32a19141e949
Matheus Fabião <matheusfabiao.dev@gmail.com>
2024-09-17 20:56:07 -0300

    feat: topics page added

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     """Página principal do Learning Log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
