cae49deab182514a959ae73647cc8dff791cb97f
Matheus Fabião <matheusfabiao.dev@gmail.com>
2024-10-11 12:51:20 -0300

    feat: new_entry from view added

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -39,3 +39,24 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Acrescenta uma nova entrada para um assunto em particular."""
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formulário em branco
+        form = EntryForm()
+    
+    else:
+        # Dados submetidos; processa os dados
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
